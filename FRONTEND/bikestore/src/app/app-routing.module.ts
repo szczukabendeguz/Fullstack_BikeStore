8d2d08a0692d2de7837548931d9dd90a1e16947f
szczukabendeguz <szczukabendeguz@gmail.com>
2025-05-17 19:47:00 +0200

    defined routing for the navbar

--- FRONTEND/bikestore/src/app/app-routing.module.ts
+++ FRONTEND/bikestore/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListBrandsComponent } from './list-brands/list-brands.component';
+import { ListModelsComponent } from './list-models/list-models.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/brands', pathMatch: 'full'},
+  {path: 'brands', component: ListBrandsComponent},
+  {path: 'models', component: ListModelsComponent},
+  {path: 'statistics', component: StatisticsComponent},
+  {path: '**', redirectTo: '/brands', pathMatch: 'full'}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
