789c7d9a613c9de23eeb4f6720abc1569cedf4ee
szczukabendeguz <szczukabendeguz@gmail.com>
2025-05-17 19:47:45 +0200

    created model class based on the backend api

--- FRONTEND/bikestore/src/app/model.ts
+++ FRONTEND/bikestore/src/app/model.ts
@@ -1,2 +1,21 @@
 export class Model {
+    modelName: string | null;
+    frontTravel: number;
+    backTravel: number;
+    askingPrice: number;
+    userFullName: string | null;
+
+    constructor(
+        modelName: string | null,
+        frontTravel: number,
+        backTravel: number,
+        askingPrice: number,
+        userFullName: string | null
+    ) {
+        this.modelName = modelName;
+        this.frontTravel = frontTravel;
+        this.backTravel = backTravel;
+        this.askingPrice = askingPrice;
+        this.userFullName = userFullName;
+    }
 }
